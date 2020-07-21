5ef10a9d0def738c5d55d9c50561d1c2da505f64
khasenovweb <hasenovkirill93@gmail.com>
2020-07-21 17:22:08 +0500

    sdjhsjdh

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-	var header = new Headhesive('.navbar', {
-		offset: 500
-	});
+$(document).ready(function () {
+  var header = new Headhesive(".navbar", {
+    offset: 500,
+  });
+
+  $("[data-modal-show]").click(function () {
+    var id = $(this).attr("data-modal-show");
+    $('[data-modal="' + id + '"]').show();
+  });
+  $("[data-modal-close]").click(function () {
+    var id = $(this).attr("data-modal-close");
+    $('[data-modal="' + id + '"]').hide();
+  });
+
+  $('input[name="phoneinput"]').mask("+7 (999) 999-9999");
 });
